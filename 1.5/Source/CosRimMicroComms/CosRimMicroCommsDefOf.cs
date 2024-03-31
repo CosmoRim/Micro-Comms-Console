a4ac6369616484206ce06016581ee80eab609ea8
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-31 14:43:29 +0100

    Child Learning Fix

---
+++ 1.5/Source/CosRimMicroComms/CosRimMicroCommsDefOf.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace CosRimAir
+{
+    [DefOf]
+    public static class CosRimMicroCommsDefOf
+    {
+        static CosRimMicroCommsDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(CosRimMicroCommsDefOf));
+        }
+
+        public static ThingDef CosRim_MicroCommsConsole;
+    }
+}
---
+++ 1.5/Source/CosRimMicroComms/Harmony/Patch_LearningGiver_Radiotalking_TryFindCommsConsole.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using HarmonyLib;
+
+namespace CosRimAir
+{
+    [HarmonyPatch(typeof(LearningGiver_Radiotalking), "TryFindCommsConsole")]
+    public static class Patch_LearningGiver_Radiotalking_TryFindCommsConsole
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Pawn pawn, ref Thing commsConsole, ref bool __result)
+        {
+            if(commsConsole == null)
+            {
+                commsConsole = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForDef(CosRimMicroCommsDefOf.CosRim_MicroCommsConsole), PathEndMode.InteractionCell, TraverseParms.For(pawn), 9999f, (Thing t) => t is Building_CommsConsole building_CommsConsole && building_CommsConsole.CanUseCommsNow && pawn.CanReserve(building_CommsConsole) && !building_CommsConsole.IsForbidden(pawn));
+                __result = commsConsole != null;
+            }
+        }
+    }
+}
